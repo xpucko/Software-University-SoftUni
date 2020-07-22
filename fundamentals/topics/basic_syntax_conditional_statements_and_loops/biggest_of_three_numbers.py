98fed120b5d6701c694cae417eb0755f31e539d7
Valchev <hristiyan.plamenov.valchev@gmail.com>
2020-07-23 02:27:30 +0300

    update

--- fundamentals/topics/basic_syntax_conditional_statements_and_loops/biggest_of_three_numbers.py
+++ fundamentals/topics/basic_syntax_conditional_statements_and_loops/biggest_of_three_numbers.py
@@ -3,14 +3,3 @@ def biggest_of_three_numbers(x, y, z):
 
 
 print(biggest_of_three_numbers(int(input()), int(input()), int(input())))
-
-# num1 = int(input())
-# num2 = int(input())
-# num3 = int(input())
-#
-# if num1 > num2 and num1 > num3:
-#     print(num1)
-# elif num2 > num1 and num2 > num3:
-#     print(num2)
-# else:
-#     print(num3)
