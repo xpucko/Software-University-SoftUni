1f4b3ac8b4cef82130768c06435235c25022617e
Valchev <hristiyan.plamenov.valchev@gmail.com>
2020-07-26 00:33:23 +0300

    update

--- advanced/exams/february_2020/repeating_dna.py
+++ advanced/exams/february_2020/repeating_dna.py
@@ -1,10 +1,8 @@
-def get_repeating_dna(string):
+def get_repeating_DNA(string):
     d = {}
     result = []
     for i in range(len(string)):
         substring = string[i:i + 10]
         d[substring] = d.get(substring, 0) + 1
-    for key, value in d.items():
-        if value > 1:
-            result.append(key)
+    [result.append(key) for key, value in d.items() if value > 1]
     return result
