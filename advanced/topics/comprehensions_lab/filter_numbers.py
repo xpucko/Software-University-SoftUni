ed3266237d165d4bef30b15a7cdf1c28f738f101
Valchev <hristiyan.plamenov.valchev@gmail.com>
2020-09-24 19:25:35 +0300

    added new solve for problem filter_number

--- advanced/topics/comprehensions_lab/filter_numbers.py
+++ advanced/topics/comprehensions_lab/filter_numbers.py
@@ -1,6 +1,1 @@
-def is_valid(number):
-    results = [x for x in range(2, 11) if number % x == 0]
-    return True if results else False
-
-
-print([x for x in range(int(input()), int(input()) + 1) if is_valid(x)])
+print([x for x in range(int(input()), int(input()) + 1) if len([n for n in range(2, 11) if x % n == 0])])
