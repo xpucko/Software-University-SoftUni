9a3e111d89385e236ce8e86c1d5727c6671e95c3
Valchev <hristiyan.plamenov.valchev@gmail.com>
2020-09-24 19:30:06 +0300

    add new solve for filter_number

--- advanced/topics/comprehensions_lab/filter_numbers.py
+++ advanced/topics/comprehensions_lab/filter_numbers.py
@@ -1,1 +1,1 @@
-print([x for x in range(int(input()), int(input()) + 1) if len([n for n in range(2, 11) if x % n == 0])])
+print([x for x in range(int(input()), int(input()) + 1) if any([x % n == 0 for n in range(2, 11)])])
