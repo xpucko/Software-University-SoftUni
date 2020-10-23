a825d8e64c8670cea18259ca777c923aa72cc194
Valchev <hristiyan.plamenov.valchev@gmail.com>
2020-10-24 00:29:41 +0300

    deleted useless folder

--- advanced/exams/february_2020/repeating_dna.py
+++
@@ -1,8 +0,0 @@
-def get_repeating_DNA(string):
-    d = {}
-    result = []
-    for i in range(len(string)):
-        substring = string[i:i + 10]
-        d[substring] = d.get(substring, 0) + 1
-    [result.append(key) for key, value in d.items() if value > 1]
-    return result
